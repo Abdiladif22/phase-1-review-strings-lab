cfe1329cb31b9eb31f301d6ad243756a6b6905f3
Abdiladi22 <abdiladifali0@gmail.com>
2023-03-19 13:35:06 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper currentUse!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER currentUse!';
+const shortGreeting = 'Welcome, G currentUse!';
